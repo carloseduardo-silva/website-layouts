b958d6c198ad915e618053855fee8a722ec029e4
Carlos Eduardo da Silva <carloseduardokadu2004@gmail.com>
2024-03-07 00:09:30 -0300

    fix: Fixing entrance sections animations + checking responsivity + new buttons

---
+++ restaurante-template/indexController.js
@@ -0,0 +1,79 @@
+class indexController {
+
+    constructor(){
+
+        this.sections = document.querySelectorAll('.notShow')
+        this.fatos = document.querySelector('#fatos-items')
+        this.fatosContainer = document.querySelector('#Fatos')
+        this.counterYear = document.querySelector('#counterYear')
+        this.counterClient = document.querySelector('#counterClient')
+        this.counterMenu = document.querySelector('#counterMenu')
+
+
+        this.init()
+      
+    }
+
+
+
+    init(){
+        this.observe()
+       
+
+    }
+
+
+    observe(){
+        const observer = new IntersectionObserver((entries) =>{
+            if(entries[0].isIntersecting){
+
+                entries[0].target.classList.add('show')
+                entries[0].target.classList.remove('notShow')
+            }
+            else{
+                entries[0].target.classList.add('notShow')
+                entries[0].target.classList.remove('show')
+            }
+        })
+        
+        this.sections.forEach((section) => {
+            observer.observe(section)
+        })
+
+        
+        const counterObserer = new IntersectionObserver((entries) =>{
+            if(entries[0].isIntersecting){
+                console.log(entries)
+
+                this.countingAnimation(1, 10, 1, 320, this.counterYear)
+                this.countingAnimation(0, 1000, 10, 30, this.counterClient)
+                this.countingAnimation(0, 50, 1, 50, this.counterMenu)
+            }
+        })
+
+        counterObserer.observe(this.fatosContainer)
+       
+    }
+
+
+    countingAnimation(initial, end, add, time, el){
+
+
+        function increment(i, max){
+            if(i > max) return;
+            setTimeout(function(){
+             el.innerText = '+ ' + i
+            increment(i+add, max)
+            }, time)
+
+        }
+        increment(initial, end)
+
+
+    }
+    
+}
+
+
+
+
